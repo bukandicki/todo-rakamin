f06a2b0b31dac88c9ff00e41351e61a074a8eb4b
Dicki <bukandicki@gmail.com>
2022-08-31 21:45:54 +0700

    styles: Update Home styling and some logic

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -1,12 +1,40 @@
+import { useContext } from "react"
+import { RootContext } from "lib/context"
+
+import TaskCard from "components/TaskCard/TaskCard.component"
+
 import styles from "./Home.module.sass"
 
-console.log(styles);
-
 const Home = () => {
+    const { todos } = useContext(RootContext)
+
+    const variants = ["primary", "success", "warning", "danger"]
+
+    const renderTodos = () => {
+        return todos.map((todo, idx) => {
+            const taskLeft = todos[idx - 1]?.id
+            const taskRight = todos[idx + 1]?.id
+            const getRandomVariant = variants[Math.floor(Math.random() * variants.length)]
+
+            return (
+                <TaskCard
+                    key={todo.id}
+                    title={todo.title}
+                    taskLeft={taskLeft}
+                    taskRight={taskRight}
+                    desc={todo.description}
+                    variant={getRandomVariant}
+                    todoId={todo.id}
+                />
+            )
+        })
+    }
+
     return (
         <main className={styles.HomePage}>
-            <section className={styles.HomePage__section}>
-                
+            <section className={styles.HomePage_section}>
+                {todos && renderTodos()}
+                {!todos.length && <h1>Loading...</h1>}
             </section>
         </main>
     )
