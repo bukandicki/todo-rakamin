a1d77d4f2d30165a680dd29484841b613be67575
Dicki <bukandicki@gmail.com>
2022-08-31 21:49:17 +0700

    feat: Improve navbar components

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,18 +1,37 @@
+import { useComponentVisible } from "utils/clickOutsideElement"
+import { createTodo } from "lib/api"
 import { ReactComponent as PlusIcon } from "icons/plus.svg"
 
 import Form from "components/Form/Form.component"
 import Modal from "components/Modal/Modal.component"
 
 import styles from "./Navbar.module.sass"
-import { useComponentVisible } from "utils/clickOutsideElement"
-import { createTodo } from "lib/api"
 
-const Navbar = () => {
+const Navbar = ({ onUpdated }) => {
+    let timeout
     const { isComponentVisible, setIsComponentVisible } = useComponentVisible(false)
 
     const handleSubmit = async ({ title, description }) => {
-        await createTodo({ title, description })
-        setIsComponentVisible(false)
+        clearTimeout(timeout)
+        timeout = setTimeout(async () => {
+            await createTodo({ title, description })
+            await onUpdated()
+            setIsComponentVisible(false)
+        }, 500)
+    }
+
+    const buttonConfig = {
+        cancel: {
+            title: "Cancel",
+            handler: () => setIsComponentVisible(false)
+        },
+        confirm: {
+            title: "Create Group",
+            styles: {
+                backgroundColor: "#01959F",
+                color: "#fff"
+            }
+        }
     }
 
     return (
@@ -22,7 +41,7 @@ const Navbar = () => {
                 show={isComponentVisible}
                 onClose={() => setIsComponentVisible(false)}
             >
-                <Form onSubmit={handleSubmit} common={false} />
+                <Form button={buttonConfig} onSubmit={handleSubmit} common={false} />
             </Modal>
             <h1 className={styles.Navbar_title}>Product Roadmap</h1>
             <button className={styles.Navbar_button} onClick={() => setIsComponentVisible(true)}>
